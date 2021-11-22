d25d877cc85d587518667452a847e8bdf8cdcc06
NooraJumppanen <noora.jumppanen@gmail.com>
2021-11-22 15:08:54 +0200

    made country flags with data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [],
+  }
+
+  componentDidMount(){
+    axios
+    .get("https://restcountries.com/v3.1/all").then(res => {this.setState({data: res.data});
+    });
+  }
+
+  render() {
+    return (
+      <>
+      <div className="countries">
+        {this.state.data.map(c =>
+        <div className="country"
+          key={c.name.common}>
+          <h2>{c.name.common}</h2>
+          <p><strong>Capital:</strong> {c.capital}</p>
+          <p><strong>Population:</strong> {c.population}</p> 
+          <img src={c.flags.svg} alt="flag" className="flag"/>
+      </div>
+      )}
+      </div>
+      </>
+    );
+  }
 }
 
 export default App;
